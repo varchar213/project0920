b51941904104fa712c1b30159e7f12329593ec22
varchar213 <jmcs7eg1@gmail.com>
2023-09-20 17:10:53 +0900

    올리기

---
+++ project_0920_1/src/main/java/org/kosta/myproject/Project09201Application.java
@@ -0,0 +1,13 @@
+package org.kosta.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project09201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project09201Application.class, args);
+	}
+
+}
